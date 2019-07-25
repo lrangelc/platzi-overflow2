3f9920bc019f24368af212e3c53bbdde8c9c6432
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-07-25 08:06:37 -0600

    Dando estilo al formulario de preguntas

--- src/app/question/question.model.ts
+++ src/app/question/question.model.ts
@@ -1,8 +1,11 @@
+import { Answer } from '../answer/answer.model';
+
 export class Question {
     title: string;
     description: string;
     createdAt?: Date;
     icon?: string;
+    answers: Answer[]
 
     constructor(title: string,
         description: string,
@@ -13,6 +16,6 @@ export class Question {
         this.description = description;
         this.createdAt = createdAt;
         this.icon = icon;
-
+        this.answers = [];
     }
 }
