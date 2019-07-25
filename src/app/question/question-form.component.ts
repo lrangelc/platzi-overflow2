c011f0a023e73c24fb9b25a77fe8194648272416
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-07-25 15:44:06 -0600

    Crear la pantalla para cargar una nueva pregunta

---
+++ src/app/question/question-form.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-question-form',
+    templateUrl: './question-form.component.html'
+})
+
+export class QuestionFormComponent{
+
+}
