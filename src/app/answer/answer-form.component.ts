9e04c0bc154cf7ec87d854d020ee7ff74feb8182
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-07-25 08:40:52 -0600

    Cómo mostrar las respuestas en el listado

--- src/app/answer/answer-form.component.ts
+++ src/app/answer/answer-form.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Answer,User } from './answer.model';
+import { Question } from '../question/question.model';
 
 @Component({
     selector: 'app-answer-form',
@@ -10,14 +12,22 @@ import { NgForm } from '@angular/forms';
             flex-direction: column;
             width: 90%;
             margin: 40px auto;
-            margin-top: 20px;
-            
+            margin-top: 20px;            
         }
     `]
 })
 
 export class AnswerFormComponent {
+    @Input() question : Question;
+
     onSubmit(form: NgForm) {
         console.log(form.value.description);
+        const answer = new Answer(
+            form.value.description,
+            this.question,
+            new Date(),
+            new User('Paula','Becerra'));
+        this.question.answers.unshift(answer);
+        form.reset();
     }
 }
