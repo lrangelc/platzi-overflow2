dcfadc855adc32f4a9a43b87f0b1ddf3900f7212
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-07-25 18:07:12 -0600

    Conectando el listado de preguntas con la pantalla del detalle

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { QuestionListComponent } from './question/question-list.component';
 import { SigninScreenComponent } from './auth/signin-screen.component';
 import { SignupScreenComponent } from './auth/signup-screen.component';
+import { QUESTION_ROUTES } from './question/question.routing';
 
 const APP_ROUTES: Routes = [
     {
@@ -16,6 +17,10 @@ const APP_ROUTES: Routes = [
     {
         path: 'signup',
         component: SignupScreenComponent
+    },
+    {
+        path: 'questions',
+        children: QUESTION_ROUTES
     }
 ];
 
