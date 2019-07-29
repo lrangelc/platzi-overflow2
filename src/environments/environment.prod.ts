034c2d0ab9cbe26f5b4b43af44a3201197513992
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-07-28 23:01:09 -0600

    fix url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://platzi-overflow2-backend.herokuapp.com///api/'
+  apiUrl: 'https://platzi-overflow2-backend.herokuapp.com/api/'
 };
